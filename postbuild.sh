07b4ee2a28633b85dea79760b752c7f5b1b9bdbe
Nyxchrono <154691752+Nyxchrono@users.noreply.github.com>
2024-01-05 20:53:20 -0500

    Update `postbuild.sh`

--- postbuild.sh
+++ postbuild.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 install $1 /home/nyxchrono/.config/r2modmanPlus-local/LethalCompany/profiles/Development/BepInEx/plugins/Testing/
-install $1 ./Thunderstore/
+install $1 ../Thunderstore/
 exit 0
